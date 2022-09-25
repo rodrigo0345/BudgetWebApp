c68c40091e6382d2ee68891347767b29feef2e16
rodrigo0345 <rodrigocralha@gmail.com>
2022-09-25 18:01:57 +0100

    main page html

--- vite-project/src/components/Main.tsx
+++ vite-project/src/components/Main.tsx
@@ -1,7 +1,65 @@
 import React from 'react'
 
 export default function Main() {
+  // set by default to a month 
+  const [income, setIncome] = React.useState(0);
+  const [expense, setExpense] = React.useState(0);
+
   return (
-    <div>Main</div>
+    <>
+      <header className="header">
+        <h1>Budget App</h1>
+        <ul>
+          <li>Login</li>
+          <li>New movement</li>
+        </ul>
+      </header>
+      <main>
+        <div className="intro">
+          <h1>Portfolio</h1>
+          <div className="income">
+            <div className="result">
+              <h2>Income</h2>
+              <p>23.00€</p>
+            </div>
+            <ul className="income__list">
+                <li className='income__item'>
+                  <div className="income__item--description">
+                    <p>Salary</p>
+                  </div>
+                  <div className="income__item--value">
+                    <p>23.00€</p>
+                  </div>
+                </li>
+            </ul>
+          </div>
+          <div className="spending">
+            <div className="result">
+              <h2>Spending</h2>
+              <p>23.00€</p>
+            </div>
+            <ul className="spending__list">
+                <li className='spending__item'>
+                  <div className="spending__item--description">
+                    <p>Food</p>
+                  </div>
+                  <div className="spending__item--value">
+                    <p>23.00€</p>
+                  </div>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <footer>
+          <div className="contact">
+            <h2>Contact</h2>
+            <p>email: <a href="mailto:casanova.rodrigo0011">Developer</a></p>
+          </div>
+          <div className="copyright">
+            <p>© 2022 Rodrigo Casanova</p>
+          </div>
+      </footer>
+    </>
   )
 }
